ae90549b86d7f4940f61e32ac37e6eeabec8fbb3
rddolor27 <rddolor22@gmail.com>
2026-07-19 14:44:12 +0800

    chore: migrate to vite

--- portfolio-site/README.md
+++ portfolio-site/README.md
@@ -1,70 +1,34 @@
-# Getting Started with Create React App
+# Portfolio Site
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Personal portfolio built with **React + Vite**, **Tailwind CSS**, and **shadcn/ui**-style components, with **Framer Motion** animations and a tsparticles background.
 
 ## Available Scripts
 
 In the project directory, you can run:
 
-### `npm start`
+### `npm run dev`
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
-
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+Runs the app in development mode with Vite.\
+Open [http://localhost:5173/portfolio/](http://localhost:5173/portfolio/) to view it in your browser. The page hot-reloads when you make changes.
 
 ### `npm run build`
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+Builds the app for production to the `dist` folder.
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+### `npm run preview`
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+Serves the production build locally for a final check before deploying.
 
-### `npm run eject`
+### `npm run deploy`
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+Builds and publishes the `dist` folder to the `gh-pages` branch (GitHub Pages).
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+## Stack
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+- [Vite](https://vitejs.dev/) — build tool and dev server
+- [React 18](https://react.dev/)
+- [Tailwind CSS](https://tailwindcss.com/) with shadcn/ui-style components in `src/components/ui/`
+- [Framer Motion](https://www.framer.com/motion/) — section reveal animations
+- [Embla Carousel](https://www.embla-carousel.com/) — project image carousels
+- [EmailJS](https://www.emailjs.com/) — contact form
+- [tsparticles](https://particles.js.org/) — animated background
--- portfolio-site/src/data/projectData.js
+++ portfolio-site/src/data/projectData.js
@@ -1,3 +1,22 @@
+import ClusteraUpload from '../assets/project_images/Clustera-images/UploadPage.JPG';
+import ClusteraLDA1 from '../assets/project_images/Clustera-images/LDApage1.JPG';
+import ClusteraLDA2 from '../assets/project_images/Clustera-images/LDApage2.JPG';
+import ClusteraLDA3 from '../assets/project_images/Clustera-images/LDApage3.JPG';
+import ClusteraLDA4 from '../assets/project_images/Clustera-images/LDApage4.JPG';
+import ClusteraLDA5 from '../assets/project_images/Clustera-images/LDApage5.JPG';
+import ClusteraLDA6 from '../assets/project_images/Clustera-images/LDApage6.JPG';
+import ClusteraLDA7 from '../assets/project_images/Clustera-images/LDApage7.JPG';
+import ClusteraLDA8 from '../assets/project_images/Clustera-images/LDApage8.JPG';
+import ClusteraLDA9 from '../assets/project_images/Clustera-images/LDApage9.JPG';
+import ClusteraLDA10 from '../assets/project_images/Clustera-images/LDApage10.JPG';
+import ClusteraLDA11 from '../assets/project_images/Clustera-images/LDApage11.JPG';
+import TestImage from '../assets/project_images/Test.JPG';
+import Interpreter1 from '../assets/project_images/LOLCode-Interpreter-images/interpreter1.png';
+import TrackIt1 from '../assets/project_images/TrackIt-images/TrackItPage1.JPG';
+import TrackIt2 from '../assets/project_images/TrackIt-images/TrackItPage2.JPG';
+import TrackIt3 from '../assets/project_images/TrackIt-images/TrackItPage3.JPG';
+import TrackIt4 from '../assets/project_images/TrackIt-images/TrackItPage4.JPG';
+
 const data = {
     name: '',
     image: '',
@@ -15,19 +34,18 @@ const data = {
             name: 'Clustera',
             images:
                 [
-                    require('../assets/project_images/Clustera-images/UploadPage.JPG'),
-                    require('../assets/project_images/Clustera-images/LDApage1.JPG'),
-                    require('../assets/project_images/Clustera-images/LDApage2.JPG'),
-                    require('../assets/project_images/Clustera-images/LDApage3.JPG'),
-                    require('../assets/project_images/Clustera-images/LDApage4.JPG'),
-                    require('../assets/project_images/Clustera-images/LDApage5.JPG'),
-                    require('../assets/project_images/Clustera-images/LDApage6.JPG'),
-                    require('../assets/project_images/Clustera-images/LDApage7.JPG'),
-                    require('../assets/project_images/Clustera-images/LDApage8.JPG'),
-                    require('../assets/project_images/Clustera-images/LDApage9.JPG'),
-                    require('../assets/project_images/Clustera-images/LDApage10.JPG'),
-                    require('../assets/project_images/Clustera-images/LDApage11.JPG'),
-
+                    ClusteraUpload,
+                    ClusteraLDA1,
+                    ClusteraLDA2,
+                    ClusteraLDA3,
+                    ClusteraLDA4,
+                    ClusteraLDA5,
+                    ClusteraLDA6,
+                    ClusteraLDA7,
+                    ClusteraLDA8,
+                    ClusteraLDA9,
+                    ClusteraLDA10,
+                    ClusteraLDA11,
                 ],
             about:
                 "A web application made with React and Django that serves as a tool for document or textual analysis, utilizing topic techniques for classification, clustering and generation of topics. This is a Special Problem which is one of the finalrequirements to finish the Computer Science program of UPLB, which is equivalent to a thesis or Capstone Project.",
@@ -40,7 +58,7 @@ const data = {
         },
         {
             name: 'Social Media Application',
-            images: [require('../assets/project_images/Test.JPG'), require('../assets/project_images/Test.JPG'), require('../assets/project_images/Test.JPG'), require('../assets/project_images/Test.JPG')],
+            images: [TestImage, TestImage, TestImage, TestImage],
             about:
                 'A Social Media application mirroring Facebook which can create, update, delete, and read posts. Created using React.js for the frontend and Node.js and Express for the backend, implementing full CRUD operations.',
             tech: ['React', 'Node.js', 'Express.js',],
@@ -51,7 +69,7 @@ const data = {
         },
         {
             name: 'LOL Code Interpreter',
-            images: [require('../assets/project_images/LOLCode-Interpreter-images/interpreter1.png')],
+            images: [Interpreter1],
             about: 'An Interpreter made with Python for the programming language LOL. It has a Graphical User Interface, Lexical Analyzer and Syntax Analyze',
             tech: ['Python'],
             links: {
@@ -63,10 +81,10 @@ const data = {
         {
             name: 'Financial Tracker',
             images: [
-                require('../assets/project_images/TrackIt-images/TrackItPage1.JPG'),
-                require('../assets/project_images/TrackIt-images/TrackItPage2.JPG'),
-                require('../assets/project_images/TrackIt-images/TrackItPage3.JPG'),
-                require('../assets/project_images/TrackIt-images/TrackItPage4.JPG'),
+                TrackIt1,
+                TrackIt2,
+                TrackIt3,
+                TrackIt4,
             ],
             about: "TrackIt: An online web application that allows users to track and visualize their overall expenses. The application is built using Next.js and Tailwind CSS for the frontend, with D3.js for visualization components. The backend is developed using Java Spring Boot, providing RESTful API services along with JWT authentication and authorization. PostgreSQL is used as the database to store user data and expenses.",
             tech: ['Next.js', 'D3.js', 'Tailwind', 'Spring Boot', 'PostgreSQL'],
